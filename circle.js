3e818986b98f6e792b33745cc8f97707348054f6
morganecf <morganeciot@gmail.com>
2015-06-09 21:17:14 -0400

    whyyyyy doesnt the circle behave according to conventional and accepted and safe laws of physics!!!!!!

--- circle.js
+++ circle.js
@@ -3,17 +3,41 @@ var v = cp.v;
 var GRABABLE_MASK_BIT = 1<<31;
 var NOT_GRABABLE_MASK = ~GRABABLE_MASK_BIT;
 
-var socket = io();
-
 // NOTE: hue places color on color wheel - 0 is red, 120 is green, 240 is blue, 360 red again 
 
 var CircleDemo = function () {
+
+	var point2canvas = function(point) {
+			return new Point(point.x, (height - point.y));
+	};
+
+	var canvas2point = function(x, y) {
+		return v(x, height - y);
+	};
+
+	var width = window.innerWidth-30;
+	var height = window.innerHeight-30;
+
+	// The socket client 
+	var socket = io();
+
 	// Install view
 	paper.install(window);  
   	paper.setup('canvas');
 
+  	// Create the user 
+	var user = new Path.Circle(new Point(0, 0), 35);
+	// Customizing you 
+	user.fillColor = 'grey';
+	user.strokeColor = 'black';
+	user.strokeWidth = 5;
+	user.opacity = 0.7;
+
 	// Create the circle
 	var circle = new Path.Circle(new Point(500, 200), 60);
+
+	console.log(canvas2point(500, 200));
+
 	// Customize it 
 	circle.fillColor = {
 		hue: 60,
@@ -25,4 +49,25 @@ var CircleDemo = function () {
 	circle.opacity = 0.7;
 
 	view.draw();
+
+	// socket.on('user-move-backend', function (move) {
+	// 	console.log('received user move event');
+	// 	user.position = new Point(move.x, move.y);
+	// 	view.draw();
+	// });
+
+	// socket.on('draw', function (data) {
+	// 	user.position = new Point(data.user);
+	// 	circle.position = new Point(data.circle);
+	// });	
+
+	socket.on('draw', function (data) {
+		user.position = new Point(point2canvas(data.circle_data[1]));
+		circle.position = new Point(point2canvas(data.circle_data[0]));
+
+		//console.log(user.position);
+		//console.log(circle.position);
+
+		view.draw();
+	});	
 };
--- engine.js
+++ engine.js
@@ -104,36 +104,126 @@ function start_ball_simulation () {
 	}, 33);
 }
 
+function init_test_simulation () {
+	// Quick test: one static ball, one moving ball 
+
+	var space = new cp.Space();
+	space.iterations = 20;
+	// space.gravity = cp.v(0, -100);
+	space.gravity = cp.v(-100, 0);
+	space.sleepTimeThreshold = 0.5;
+	// space.collisionSlop = 0.5;
+	space.sleepTimeThreshold = 0.5;
+
+	//add floor
+	var pts = [cp.v(0, 0), cp.v(width, 0)];
+	var floor = space.addShape(new cp.SegmentShape(space.staticBody, pts[0], pts[1], 0));
+	console.log(floor);
+	floor.setElasticity(1);
+	floor.setFriction(0);
+	floor.setLayers(NOT_GRABABLE_MASK);
+	  
+	//add walls
+	pts = [cp.v(0, 0), cp.v(0, height), cp.v(width, 0), cp.v(width, height)];
+	var wall1 = space.addShape(new cp.SegmentShape(space.staticBody, pts[0], pts[1], 0));
+	wall1.setElasticity(1);
+	wall1.setFriction(1);
+	wall1.setLayers(NOT_GRABABLE_MASK);
+
+	var wall2 = space.addShape(new cp.SegmentShape(space.staticBody, pts[2], pts[3], 0));
+	wall2.setElasticity(1);
+	wall2.setFriction(1);
+	wall2.setLayers(NOT_GRABABLE_MASK);
+
+	var mass1 = radius1 = 100;
+	// var moment = cp.momentForCircle(mass1, 0, radius1, cp.v(0, 0));
+	var moment = .0000001;
+	console.log(moment);
+	var body1 = space.addBody(new cp.Body(mass1, moment));
+	var circle1 = space.addShape(new cp.CircleShape(body1, radius1, cp.v(500, 200)));
+	circle1.setElasticity(0.8);
+	circle1.setFriction(0);
+
+	var mass2 = radius2 = 50;
+	var body2 = space.addBody(new cp.Body(mass2, moment));
+	var circle2 = space.addShape(new cp.CircleShape(body2, radius2, cp.v(800, 200)));
+	circle2.setElasticity(0.8);
+	circle2.setFriction(0);
+
+	return {'space': space, 'circles': [circle1, circle2]};
+}
+
+function start_test_simulation () {
+	simulation = init_test_simulation();
+	setInterval(function () {
+		// Step through simulation 
+		simulation.space.step(1/30);
+
+		//console.log(simulation.circles[0].tc);
+
+		// Send positional data to front end at 30 hz
+		var data = [];
+		data.push({x: simulation.circles[0].tc.x, y: simulation.circles[0].tc.y});
+		data.push({x: simulation.circles[1].tc.x, y: simulation.circles[1].tc.y});
+		io.emit('draw', {'circle_data': data});
+
+	}, 33);
+}
+
+
 function init_circle_simulation () {
+
+	var simulation = {};
 
 	// Init the chipmunk space 
 	var space = new cp.Space();
 
-	// Create the circle
-	var radius = mass = 60;
+	// Create the user - connected to device 
+ 	var user_radius = 35;
+ 	var user_mass = 100;
+	var user_body = space.addBody(new cp.Body(user_mass, cp.momentForCircle(user_mass, 0, user_radius, cp.v(0, 0))));
+	var user = space.addShape(new cp.CircleShape(user_body, user_radius, cp.v(0, 0)));
 
-	var circle_body = space.addBody(new cp.Body(mass, cp.momentForCircle(mass, 0, radius, cp.v(0, 0))));
-	var circle = space.addShape(new cp.CircleShape(circle_body, radius, cp.v(0, 0)));
+	// Create the circle
+	var circle_radius = circle_mass = 60;
+	var circle_body = space.addBody(new cp.Body(circle_mass, cp.momentForCircle(circle_mass, 0, circle_radius, cp.v(0, 0))));
+	var circle = space.addShape(new cp.CircleShape(circle_body, circle_radius, cp.v(0, 0)));
 	circle.setElasticity(0.8);
 	circle.setFriction(1);
+
+	// Return a simulation object
+	simulation.space = space;
+	simulation.user = user;
+	simulation.circle = circle;
+	return simulation
 }
 
-function start_circle_simulation () {
-	simulation = init_circle_simulation();
-}
 
 // Listen for connection events 
 io.on('connection', function (socket) {
-  console.log('a user connected');
+	console.log('a user connected');
 
-  socket.on('ready', function () {
-    console.log("frontend is ready");
+	socket.on('ready', function () {
+		console.log("frontend is ready");
+		// Start the simulation 
+		// start_ball_simulation();
 
-    // Start the simulation 
-    // start_ball_simulation();
-    start_circle_simulation();
+		start_test_simulation();
 
-  });
+	});
+
+  	// Listen for the user moving 
+	// socket.on('user-move', function (move) {
+
+	// 	// Update backend position 
+	// 	simulation.user.tc.x = move.x;
+	// 	simulation.user.tc.y = move.y;
+
+	// 	// Send message to update frontend position in the demo 
+	// 	// (not the emulator - that's already been updated)
+	// 	io.emit('user-move-backend', move);
+
+	// });
 
 });
 
