c44d6e7c791955b5575352158e5b8bfdb0e68b35
morganecf <morganeciot@gmail.com>
2015-06-18 14:59:07 -0400

    eel locomotion down

---
+++ aqua-engine.js
@@ -0,0 +1,145 @@
+/* 
+	Simple slingshot example using Chipmunk JS. 
+*/
+
+// Server/socket stuff 
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.use(express.static(__dirname));
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/slingshot.html');
+});
+
+// Import the physics library 
+var cp = require('chipmunk');
+
+// Constants 
+var GRABABLE_MASK_BIT = 1<<31;
+var NOT_GRABABLE_MASK = ~GRABABLE_MASK_BIT;
+
+var simulation_timestep = 1.0 / 60.0;			// 1/seconds
+var interval_step = 66;							// milliseconds 
+
+// Global variables
+var arena, space, body, spring_body;
+var width, height, spring_height;
+var arm_size = 300;
+var constraints = {};
+// Will hold the interval id 
+var simulation = null;
+
+function slingshot () {
+
+	running = true;
+
+	// Create space 
+	space = new cp.Space();
+	space.gravity = cp.v(0, 0);
+	space.collisionSlop = 0.5;
+
+	// Function for slingshot parts 
+	var segment = function (x1, y1, x2, y2) {
+  		var seg = space.addShape(new cp.SegmentShape(space.staticBody, cp.v(x1, y1), cp.v(x2, y2), 0));
+		seg.setFriction(1);
+		seg.setElasticity(1);
+		seg.setLayers(NOT_GRABABLE_MASK);
+		return segment;
+  	};
+
+  	// The ground and two slingshot arms 
+  	var ground = segment(arena.ground.x1, arena.ground.y1, arena.ground.x2, arena.ground.y2);
+ 	var armL = segment(arena.armTop.x1, arena.armTop.y1, arena.armTop.x2, arena.armTop.y2);
+ 	var armR = segment(arena.armBottom.x1, arena.armBottom.y1, arena.armBottom.x2, arena.armBottom.y2);
+
+ 	space.setDefaultCollisionHandler(null, null, function (arbiter, space) {
+ 		console.log('collision');
+ 	});
+
+	// Step through the simulation 
+	simulation = setInterval(function () {
+		if (body) {
+			var pos = body.getPos();
+
+			/* HI RICHARD THIS IS WHERE I PRINT OUT THE SPRING FORCE */
+			if (constraints.spring_on) {	
+				var delta = cp.v.sub(pos, spring_body.p);
+				var dist = cp.v.len(delta);
+				var n = cp.v.mult(delta, 1/(dist ? dist : Infinity));
+				var f_spring = constraints.spring.springForceFunc(constraints.spring, dist);
+				var spring_force = {x: n.x * f_spring, y: n.y * f_spring};
+
+				console.log('SPRING FORCE:', spring_force);
+				console.log();
+			}
+
+			// Add an offset of 5
+			//if (constraints.spring_on && pos.x > (arena.start.x + arena.bird_radius + 5)) {
+			if (constraints.spring_on && pos.x > (arena.start.x + arena.bird_radius)) {
+				console.log('release');
+				space.removeConstraint(constraints.spring);
+				constraints.spring_on = false;
+			}
+
+			space.step(simulation_timestep);
+			io.emit('draw', pos);
+		}
+	}, 66);
+}
+
+// Start the simulation as soon as front end has loaded 
+io.on('connection', function (socket) {
+
+	// Wait for the front end to be ready 
+	socket.on('ready', function (arena_info) {
+		// Kill any existing simulations 
+		if (simulation) {
+			clearInterval(simulation);
+			simulation = null;
+		}
+
+		arena = JSON.parse(arena_info);
+		width = arena.width;
+		height = arena.height;
+
+		slingshot();
+	});
+ 
+ 	// On mouse down, create the bird and spring 
+	socket.on('mousedown', function (pos) {
+		if (space) {
+			// The body we're going to throw 
+		 	var bird_start = cp.v(arena.bird_center.x, arena.bird_center.y);
+		 	var mass = 3;
+		 	var momentum = cp.momentForCircle(mass, 0, arena.bird_radius, cp.v(0, 0));
+		 	body = space.addBody(new cp.Body(mass, momentum));
+		 	body.setPos(bird_start);
+		 	var shape = space.addShape(new cp.CircleShape(body, arena.bird_radius, cp.v(0, 0)));
+
+			// Slingshot is modeled as a simplified spring attached to invisible body in the middle of the two arms
+		 	spring_body = new cp.Body(Infinity, Infinity);
+		 	spring_body.setPos(arena.start);
+		 	// TODO - world2local should be arena.start or bird-start? 
+		  	var spring = new cp.DampedSpring(spring_body, body, cp.v(0, 0), body.world2Local(arena.start), 0, 1000, 0);
+			space.addConstraint(spring);
+			constraints.spring = spring;
+			constraints.spring_on = true;
+
+			io.emit('pulling');
+		}
+	});
+
+	// Update the bird body position 
+	socket.on('mousemove', function (pos) {
+		if (body) body.setPos(cp.v(pos.x, pos.y));
+	});
+
+});
+
+// Listen to port 3000
+http.listen(3000, function () {
+    console.log("listening on *:3000");
+});
+
+
---
+++ aqua-slingshot.js
@@ -0,0 +1,328 @@
+/* Functionality for creating the slingshot */
+
+// Helper function to return a random number between two numbers
+var random = function (a, b) { 
+	return a + (Math.random() * (b - a)); 
+};
+
+// Function to create a slingshot line segment 
+var segment = function (x1, y1, x2, y2) {
+	var seg = new Path();
+	seg.strokeColor = 'coral';
+	seg.strokeWidth = 30;
+	seg.opacity = 0.8;
+	// Add the starting location 
+	seg.add(new Point(x1, y1));
+	// Add segments between start and end
+	var unit = (x2 - x1) / 5.0;
+	for (var i = 1; i < 5; i++) {
+		// Make them deviate a little in the y direction 
+		seg.add(new Point(x1 + i * unit, y1 + Math.random() * 20));
+	}
+	// Add the end location 
+	seg.add(new Point(x2, y2));
+	seg.strokeCap = 'round';
+	//seg.smooth();
+};
+
+// Function to create a coral flower 
+var flower = function (x, y) {
+	var start = new Point(x, y);
+	var end = new Point(x + Math.random() * 10, y + Math.random() * 8);
+	var stem = new Path(start, end);
+	var bud_radius = 3;
+	var bud = new Path.Circle(new Point(end.x + bud_radius, end.y + (bud_radius /2)), 3);
+	var flower_group = new Group(stem, bud);
+	flower_group.fillColor = "#FF847C";
+	flower_group.strokeColor = "#FF847C";
+	flower_group.strokeWidth = 4;
+};
+
+// Have a different setup based on chosen slingshot arm separation size
+var setup = function (size, width, height, radius, arm_size) {
+	var components = {};
+
+	// Where the spring/slingshot base starts off at 
+	components.start = {};
+
+	// Used to position the arms 
+	var mid = height / 2;
+	var a = mid + ((size / 2.0) * mid);
+	var b = mid - ((size / 2.0) * mid);
+
+	// Ground 
+	components.ground = {
+		x1: 0, y1: 0, x2: 0, y2: height,
+		create: function () { return segment(this.x1, this.y1, this.x2, this.y2); }
+	};
+
+	// Slingshot arms (made of coral)
+	components.armTop = {
+		x1: 0, y1: a, x2: arm_size, y2: a,
+		create: function () { return segment(this.x1, this.y1, this.x2, this.y2); }
+	};
+	components.armBottom = {
+		x1: 0, y1: b, x2: arm_size, y2: b,
+		create: function () { segment(this.x1, this.y1, this.x2, this.y2); }
+	};
+
+	components.flowers = function () {
+		// Add a slew of flower corals to the coral arms 
+		// Do this first so that they show up above arms 
+		for (var i = 0; i < 10; i++) {
+			// Pick a random area within the top arm 
+			var x = random(components.armTop.x1, components.armTop.x2);
+			var y = random(components.armTop.y1, components.armTop.y2);
+			flower(x, y);
+		}
+		for (var i = 0; i < 10; i++) {
+			// Pick a random area within the bottom arm 
+			var x = random(components.armBottom.x1, components.armBottom.x2);
+			var y = random(components.armBottom.y1, components.armBottom.y2);
+			flower(x, y);
+		}
+	};
+	
+	// Slingshot 
+	components.slingshot = function () {
+		var slingshot = new Path();
+		// Add an extra 15 - this is half the size of the coral arm width 
+		// allows seaweed to 'wrap around' arm 
+	  	slingshot.add(new Point(arm_size, a + 15));
+
+	  	// Where the urchin will be nestled to start with 
+	  	components.start.x = arm_size - 20;
+	  	components.start.y = a - ((a - b) / 2);
+		slingshot.apex = slingshot.add(new Point(components.start.x, components.start.y));
+
+		slingshot.add(new Point(arm_size, b - 15));
+		
+		slingshot.strokeColor = 'green';
+		slingshot.strokeWidth = 10;
+		slingshot.smooth();
+		slingshot.strokeCap = 'butt';
+		return slingshot;
+	};
+
+	// The urchin center is slightly offset from the center of the slingshot 
+	components.urchin_radius = radius;
+	components.urchin_center = null;
+	components.urchin = function () {
+		components.urchin_center = {x: components.start.x + 20, y: components.start.y - 8};
+		var urchin = new Path.Circle(new Point(components.urchin_center), components.urchin_radius);
+		urchin.fillColor = 'black';
+		return urchin;
+	};
+
+	// Fish target 
+	components.fish = function (size) {
+		var body = new Path();
+		// TODO 
+	};
+	components.fish.prototype.animate = function () {
+		// TODO 
+	};
+
+	// Eel - direction is 1 or -1 
+	components.eel = function (specs) {
+		var body = new Path(specs.p);
+		var back = new Path(specs.p);
+		// Add segments to the eel body, each of size 'size'
+		var point;
+		for (var i = 1; i < 4; i++) {
+			point = new Point(specs.p.x, specs.p.y + specs.dir * specs.size * i)
+			body.add(point);
+			back.add(point);
+		}
+		// Add the eyes 
+		var lefteye = new Path.Circle(new Point(specs.p.x - 5, specs.p.y), 6);
+		var righteye = new Path.Circle(new Point(specs.p.x + 5, specs.p.y), 6);
+		var eyes = new Group(lefteye, righteye);
+		eyes.fillColor = 'black';
+		// Add the tip of the tail 
+		var p = body.segments[3].point;
+		var end = new Point(p.x, p.y - (specs.size / 2));
+		var tail = new Path(new Point(p.x - 10, p.y), end, new Point(p.x + 10, p.y));
+		// Add a little head 
+		p = body.segments[0].point;
+		var start = new Point(p.x, p.y + 10);
+		var head = new Path(new Point(p.x - 10, p.y), start, new Point(p.x + 10, p.y));
+		// Stylings
+		var group = new Group(body, back, eyes, tail);
+		body.smooth(); back.smooth(); tail.smooth(); head.smooth();
+		body.strokeColor = 'green'; tail.fillColor = 'green'; head.fillColor = 'green';
+		back.strokeColor = 'black';
+		body.strokeWidth = 20; 
+		back.strokeWidth = 5;
+		body.opacity = 0.5; back.opacity = 0.5; tail.opacity = 0.5; eyes.opacity = 0.5; head.opacity = 0.5;
+		back.strokeCap = 'round';
+		this.body = body;
+		this.back = back;
+		this.eyes = eyes;
+		this.tail = tail;
+		this.head = head;
+		this.group = new Group(body, back, eyes, tail, head);
+		return this;
+	};
+	components.animate_eel = function (eel, time, step, start_x, width) {
+		// Start from top if we're at the bottom 
+		if (eel.tail.position.y > 800) eel.group.position.y = -100;
+
+		var segment1, segment2, sinus;
+		var top_pos, bottom_pos;
+		for (var i = 0; i < 4; i++) {
+			segment1 = eel.body.segments[i];
+			segment2 = eel.back.segments[i];
+			sinus = Math.sin(time * 2 + i*1.3);			// A cylic value between -1 and 1
+			segment1.point.x = sinus * width + start_x;	// Move the eel body segments side to side
+			segment1.point.y += step;					// Advance the eel up or down 
+			segment2.point.x = sinus * width + start_x;
+			segment2.point.y += step;
+
+			if (i == 0) top_pos = segment1.point.x;
+			if (i == 3) bottom_pos = segment1.point.x;
+		}
+		eel.eyes.position.y += step;
+		eel.eyes.position.x = top_pos;
+		eel.tail.position.y += step;
+		eel.tail.position.x = bottom_pos;
+		eel.tail.segments[1].x = top_pos;
+		eel.head.position.y += step;
+		eel.head.position.x = top_pos;
+	};
+
+	return components;
+};
+
+/* Running the slingshot simulation */
+$(document).ready(function () {
+      // Create the socket 
+      var socket = io();
+
+      // Get the device size 
+      var width = $(window).width();
+      var height = $(window).height();
+
+      // Set the canvas to the device size 
+      $("#canvas").attr("width", width);
+      $("#canvas").attr("height", height);
+
+      // Colors and gradient width percentages for the sea gradient 
+      var sea_colors = ["#092B5A", "#09738A", "#78A890", "#9ED1B7"];
+
+      // Make the background a nice gradient of blue/green
+      var background = new Path.Rectangle({
+        topLeft: new Point(0, 0), 
+        topRight: new Point(width, 0), 
+        bottomLeft: new Point(0, height), 
+        bottomRight: new Point(width, height),
+        fillColor: {
+          gradient: {stops: sea_colors},
+          origin: new Point(0, height / 2),
+          destination: new Point(width, height / 2)
+        }
+      });
+
+      var urchin_radius = 20;   // The radius of our projectile 
+      var arm_size = 250;           // The slingshot arm height 
+
+      /* Call this with a certain size to run the whole slingshot simulation */
+      function run (size) {
+        // Set up the slingshot arena 
+        var arena = setup(size, width, height, urchin_radius, arm_size);
+        //arena.ground.create();
+        arena.armTop.create();
+        arena.armBottom.create();
+        //arena.flowers();
+       	//arena.base.create();
+
+        // Create the slingshot and urchin-projectile
+        var slingshot = arena.slingshot();
+        var urchin = arena.urchin();
+
+        // Create the animated bodies, starting with some eels
+        var e1 = { p: new Point(950, 150), size: 60, dir: -1, step: 5, width: 20 };
+       	var eel1 = arena.eel(e1);
+
+        // Determines if the mouse is on the projectile
+        var touchingUrchin = function (x, y) {
+          var r = Math.pow(urchin.position.x - x, 2) + Math.pow(urchin.position.y - y, 2);
+          return r <= Math.pow(urchin_radius, 2);
+        };
+
+        arena.width = width;
+        arena.height = height;
+
+        var time = 0.0;
+        setInterval(function () {
+	      	arena.animate_eel(eel1, time, e1.step, e1.p.x, e1.width);
+	      	view.draw();
+	      	time += 0.1;
+	    }, 100);
+
+        // Tell the backend we're ready 
+      //   socket.emit('ready', JSON.stringify(arena));
+
+      //   // Functionality to drag the slingshot 
+      //   var pulling = false;
+      //   var released = false;
+      //   canvas.onmousedown = function (event) {
+      //     //event.preventDefault();
+      //     // Only send mouse down event if the mouse is touching the bird  
+      //     if (touchingUrchin(event.x, event.y)) {
+      //       socket.emit('mousedown', {x: event.x, y: event.y});
+      //     }
+      //   };
+      //   canvas.onmousemove = function (event) {
+      //     if (pulling) {
+      //       slingshot.apex.point.y = event.y;
+      //       slingshot.apex.point.x = event.x;
+      //       urchin.position.y = event.y;
+      //       urchin.position.x = event.x;
+
+      //       socket.emit('mousemove', {x: event.x, y: event.y});
+      //     }
+      //   };
+      //   canvas.onmouseup = function (event) {
+      //     pulling = false;
+      //     released = true;
+
+      //     socket.emit('mouseup', {x: event.x, y: event.y});
+      //   };
+
+      //   // As long as we're pulling, send mouse position information over 
+      //   // this prevents the ball from being released in the backend 
+      //   simulation = setInterval(function () {
+      //     if (pulling) socket.emit('mousemove', {x: urchin.position.x, y: urchin.position.y});
+      //   }, 66);
+
+      //   // Backend has determined we're dragging the bird 
+      //   socket.on('pulling', function () {
+      //     pulling = true;
+      //   });
+
+      //   socket.on('draw', function (pos) {
+      //     if (released) {
+      //       // Slingshot goes back to original position
+      //       slingshot.apex.point.y = arena.start.y;
+      //       slingshot.apex.point.x = arena.start.x;
+
+      //       // Bird goes flying according to simulation 
+      //       urchin.position.y = pos.y;
+      //       urchin.position.x = pos.x;
+
+      //       view.draw();
+      //     }
+      //   });
+      }
+
+      // Possible arena sizes 
+      var small = 0.5;
+      var medium = 1.0;
+      var large = 1.5;
+
+      // Initialize with smallest size 
+      run(medium);
+
+
+  });
